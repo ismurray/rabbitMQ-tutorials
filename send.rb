04da398d4cf302c3ab61b80d584e54d95899628c
ismurray <ians.murray@yahoo.com>
2018-05-21 18:19:02 -0400

    Configure send.rb to send 'Hello World'
    Set up producer according to the Hello World tutorial, but am getting an
    error upon running `ruby send.rb`:
    
    W, [2018-05-21T18:08:10.078668 #41923]  WARN -- #<Bunny::Session:
    0x7fd5770c2c28 guest@rabbit.local:5672, vhost=/, addresses=[rabbit.local
    :5672]>: Could not establish TCP connection to rabbit.local:5672:
    getaddrinfo: nodename nor servname provided, or not known
    /Users/ismurray/.rbenv/versions/2.4.1/lib/ruby/gems/2.4.0/gems/
    bunny-2.9.2/lib/bunny/session.rb:329:in `rescue in start': Could not
    establish TCP connection to any of the configured hosts
    (Bunny::TCPConnectionFailedForAllHosts)
    	from /Users/ismurray/.rbenv/versions/2.4.1/lib/ruby/gems/2.4.0/gems/
      bunny-2.9.2/lib/bunny/session.rb:297:in `start'
    	from send.rb:13:in `<main>'

--- send.rb
+++ send.rb
@@ -1,1 +1,31 @@
+#!/usr/bin/env ruby
 
+# require the library first
+require 'bunny'
+
+# then connect to RabbitMQ server
+# The connection abstracts the socket connection, and takes care of protocol
+# version negotiation and authentication and so on for us. Here we connect to a
+# broker on the local machine with all default settings.
+# If we wanted to connect to a broker on a different machine we'd simply specify
+# its name or IP address using the :hostname option:
+connection = Bunny.new(hostname: 'rabbit.local')
+connection.start
+
+# Next we create a channel, which is where most of the API for getting things
+# done resides:
+channel = connection.create_channel
+
+# To send, we must declare a queue for us to send to; then we can publish a
+# message to the queue:
+queue = channel.queue('hello')
+
+channel.default_exchange.publish('Hello World!', routing_key: queue.name)
+puts " [x] Sent 'Hello World!'"
+
+# Declaring a queue is idempotent - it will only be created if it doesn't exist
+# already. The message content is a byte array, so you can encode whatever you
+# like there.
+
+# Lastly, we close the connection
+connection.close
